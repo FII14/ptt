58c188696d9da815fc04b592534f806129dad423
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 18:46:09 +0700

    Update john_the_ripper.sh

--- password attack/john_the_ripper.sh
+++ password attack/john_the_ripper.sh
@@ -3,9 +3,24 @@ john_the_ripper(){
         read -p "Enter the path to the hash file: " h
         if [[ -z ${h} ]]; then
             echo "Error: zip file cannot be empty."
+            hash_file
         else
             if [[ -f ${h} ]]; then
-                sukses
+                wordlist_file(){
+                    read -p "Enter the path to the wordlist file: " w
+                    if [[ -z ${w} ]]; then
+                        echo "Error: wordlist file cannot be empty."
+                        wordlist_file
+                    else
+                        if [[ -f ${w} ]]; then
+                            sukses
+                        else
+                            echo "Error: ${w} file not found."
+                            wordlist_file
+                        fi
+                    fi
+                }
+                wordlist_file
             else
                 echo "${h} file not found."
                 hash_file
