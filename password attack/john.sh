957b24d8d5ddfc2e7483aa4ab7a6b38967d6920c
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 09:59:53 +0700

    Update john.sh

--- password attack/john.sh
+++ password attack/john.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+fcrackzip(){
+    zip_file(){
+        read -p "Enter the path to the zip file: " z
+        if [[ -z ${z} ]]; then
+            echo "Error: zip file cannot be empty."
+            zip_file
+        fi
+    }
+}
