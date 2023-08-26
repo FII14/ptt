31c12238751796af65956c8ed716ee2234e31f70
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 18:57:11 +0700

    Update john_the_ripper.sh

--- password attack/john_the_ripper.sh
+++ password attack/john_the_ripper.sh
@@ -1,18 +1,18 @@
-john_the_ripper(){
-    hash_file(){
+john_the_ripper() {
+    hash_file() {
         read -p "Enter the path to the hash file: " h
         if [[ -z ${h} ]]; then
-            echo "Error: zip file cannot be empty."
+            echo "Error: hash file cannot be empty."
             hash_file
         else
             if [[ -f ${h} ]]; then
-                format(){
+                format() {
                     read -p "Enter the format: " f
                     if [[ -z ${f} ]]; then
                         echo "Error: format cannot be empty."
                         format
                     else
-                        wordlist_file(){
+                        wordlist_file() {
                             read -p "Enter the path to the wordlist file: " w
                             if [[ -z ${w} ]]; then
                                 echo "Error: wordlist file cannot be empty."
@@ -24,7 +24,7 @@ john_the_ripper(){
                                 else
                                     echo "Error: ${w} file not found."
                                     wordlist_file
-                               fi
+                                fi
                             fi
                         }
                         wordlist_file
@@ -32,7 +32,7 @@ john_the_ripper(){
                 }
                 format
             else
-                echo "${h} file not found."
+                echo "Error: ${h} file not found."
                 hash_file
             fi
         fi
