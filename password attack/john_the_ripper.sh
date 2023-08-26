2f5cd281073a4a538c0b29ce7ff6478257ec124d
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 18:41:14 +0700

    Update john_the_ripper.sh

--- password attack/john_the_ripper.sh
+++ password attack/john_the_ripper.sh
@@ -3,6 +3,13 @@ john_the_ripper(){
         read -p "Enter the path to the hash file: " h
         if [[ -z ${h} ]]; then
             echo "Error: zip file cannot be empty."
+        else
+            if [[ -f ${h} ]]; then
+                sukses
+            else
+                echo "${h} file not found."
+                hash_file
+            fi
         fi
     }
     hash_file
