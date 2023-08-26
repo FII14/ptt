b1b841e51922cd63c7147518adae240f2c3e9f4e
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 10:07:14 +0700

    Update john.sh

--- password attack/john.sh
+++ password attack/john.sh
@@ -6,6 +6,19 @@ fcrackzip(){
         if [[ -z ${z} ]]; then
             echo "Error: zip file cannot be empty."
             zip_file
+        else
+            if [[ ! -f ${z} ]]; then
+                echo "Error: ${z} file not found."
+                zip_file
+            else
+                if [[ -f ${z} ]] && [[ ${z##*.} == "zip" ]]; then
+                    fcrackzip --help
+                    exit 0
+                else
+                    echo "${z} file is not a zip file."
+                    zip_file
+                fi
+            fi
         fi
     }
     zip_file
