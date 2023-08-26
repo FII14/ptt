4d55dd2668a8bf76518689b27c290108f8c16a8b
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-26 19:40:25 +0700

    Update john_the_ripper.sh

--- password attack/john_the_ripper.sh
+++ password attack/john_the_ripper.sh
@@ -4,7 +4,7 @@ john_the_ripper() {
     echo "########################################################################"
     echo "# John The Ripper                                                      #"                                                   
     echo "########################################################################"
-    echo "# John the Ripper is a widely used open-source password cracking tool. #"
+    echo "# John The Ripper is a widely used open-source password cracking tool. #"
     echo "# It's designed to test the security of password-protected systems by  #"
     echo "# attempting to crack password hashes using various methods, like      #"
     echo "# dictionary attacks and brute force.                                  #"
